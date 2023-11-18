7de09f34a9e871a163c747ad3fb8a15c5a47cd4f
unknown <jayocpa@gmail.com>
2023-11-18 16:02:20 +0900

    fix: change module(gspread -> st-gsheets-connection)

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-google sheets에 연동된 google forms 응답을 google cloud platform api와 python gspread library를 통해 수집
+google sheets, google forms, google cloud platform api, python, streamlit, st-gsheet-connection, pandas
+streamlit: 마이크로 웹서비스 구축 및 배포할 수 있는 framework
+st-gsheeets-connection: streamlit에서 gcp api 연결하는 python module
 수집된 응답을 pandas dataframe을 통해 편차를 구하고, 편차와 응답 제출 시간을 기준으로 정렬하여 순위를 구함
-streamlit micro web app으로 deploy
--- st_main.py
+++ st_main.py
@@ -1,23 +1,25 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import gspread as gs
+from streamlit_gsheets import GSheetsConnection
 
-def get_gs_data():
-    json_file_name = 'key.json'
-    gc = gs.service_account(filename=json_file_name)
+# def get_gs_data():
+#     json_file_name = 'key.json'
+#     gc = gs.service_account(filename=json_file_name)
 
-    sheet_key = '1Dsyp3RQ-lMAwywzktioVxsqjbvTy-OiFf-H7d4XLoYQ'
-    doc = gc.open_by_key(sheet_key)
-    worksheet = doc.worksheet('설문지 응답 시트1')
-    record_list = worksheet.get_all_records()
-    return record_list
+#     sheet_key = '1Dsyp3RQ-lMAwywzktioVxsqjbvTy-OiFf-H7d4XLoYQ'
+#     doc = gc.open_by_key(sheet_key)
+#     worksheet = doc.worksheet('설문지 응답 시트1')
+#     record_list = worksheet.get_all_records()
+#     return record_list
 
 if 'n' not in st.session_state:
     st.session_state.n = 3
 
-gs_records = get_gs_data()
-df_origin = pd.DataFrame(gs_records)
+conn = st.connection("gsheets", type=GSheetsConnection)
+df_origin = conn.read(worksheet="설문지 응답 시트1", usecols=[0,1,2])
+# gs_records = get_gs_data()
+# df_origin = pd.DataFrame(gs_records)
 
 def click_button():
     st.session_state.n -= 1
@@ -31,7 +33,7 @@ df_diff['편차'] = df_diff['몇점이 나올까요?'] - num
 
 df_result = df_diff[df_diff['편차'] > -1]
 df_result = df_result.sort_values(by=['편차', '타임스탬프'], ascending=[True, True])
-df_result.columns = ['제출일시', '이메일', '점수', '편차']
+# df_result.columns = ['제출일시', '이메일', '점수', '편차']
 n = st.session_state.n
 
 st.dataframe(df_result)

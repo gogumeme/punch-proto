ffdd44f898ffa85f5e3a80db612b68c93e7f5505
unknown <jayocpa@gmail.com>
2023-11-18 16:08:27 +0900

    fix: delete spread module that is not used

--- st_main.py
+++ st_main.py
@@ -1,25 +1,12 @@
 import streamlit as st
-import pandas as pd
-import gspread as gs
+# import pandas as pd
 from streamlit_gsheets import GSheetsConnection
-
-# def get_gs_data():
-#     json_file_name = 'key.json'
-#     gc = gs.service_account(filename=json_file_name)
-
-#     sheet_key = '1Dsyp3RQ-lMAwywzktioVxsqjbvTy-OiFf-H7d4XLoYQ'
-#     doc = gc.open_by_key(sheet_key)
-#     worksheet = doc.worksheet('설문지 응답 시트1')
-#     record_list = worksheet.get_all_records()
-#     return record_list
 
 if 'n' not in st.session_state:
     st.session_state.n = 3
 
 conn = st.connection("gsheets", type=GSheetsConnection)
 df_origin = conn.read(worksheet="설문지 응답 시트1", usecols=[0,1,2])
-# gs_records = get_gs_data()
-# df_origin = pd.DataFrame(gs_records)
 
 def click_button():
     st.session_state.n -= 1
@@ -33,7 +20,7 @@ df_diff['편차'] = df_diff['몇점이 나올까요?'] - num
 
 df_result = df_diff[df_diff['편차'] > -1]
 df_result = df_result.sort_values(by=['편차', '타임스탬프'], ascending=[True, True])
-# df_result.columns = ['제출일시', '이메일', '점수', '편차']
+df_result.columns = ['제출일시', '이메일', '점수', '편차']
 n = st.session_state.n
 
 st.dataframe(df_result)
